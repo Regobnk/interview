3b6853de92f504aabfb0dac892858c2e5e5de646
Regobnk <60800084+Regobnk@users.noreply.github.com>
2021-02-05 10:27:42 -0500

    Update httpd_interview.sh

--- httpd_interview.sh
+++ httpd_interview.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-log=testlog.log
-
-#Install httpd and configure for auto start 
+#Install httpd and configure for auto start
 sudo dnf -y update
 sudo dnf install -y httpd httpd-tools
 sudo systemctl enable httpd
 sudo systemctl start httpd
 
 
-#Configuring firewall for only port 80/443
+#Configure firewall for only port 80/443
 sudo systemctl start firewalld
 sudo firewall-cmd --zone=public --permanent --add-port=80/tcp
 sudo firewall-cmd --zone=public --permanent --add-port=443/tcp
@@ -18,35 +16,31 @@ sudo firewall-cmd --reload
 #Configure apache to set default error log location
 sudo sed -i 's/ErrorLog \"logs\/error\_log\"/ErrorLog \/var\/log\/httpd\/error\_log/' /etc/httpd/conf/httpd.conf
 
-#Possible way for logrotate to work??? Work on this as well possibly in logrotate.d/httpd
+#Configuring logrotate for error logs
 sudo echo /var/log/httpd/error_log '{' >> /etc/logrotate.d/httpd
 sudo echo daily >> /etc/logrotate.d/httpd
 sudo echo rotate 0 >> /etc/logrotate.d/httpd
 sudo echo maxage 7 >> /etc/logrotate.d/httpd
 sudo echo '}' >> /etc/logrotate.d/httpd
 
-#Install wget
+#Install wget if it's not already installed
 sudo dnf -y install wget
 
 #Install rsync
 sudo dnf -y install rsync
 sudo dnf -y install rsync-daemon
 sudo systemctl start rsyncd
-sudo systemctl enable rsyncd
+sudo systemctl enaable rsyncd
 
-#Install NTP using chrony, as ntp is no longer supported
+#Install NTP using chrony
 sudo dnf -y install chrony
 sudo systemctl start chronyd
 sudo systemctl enable chronyd
-
 
 #Remaining LAMP stack install
 sudo dnf install -y mariadb-server mariadb
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-#The following skips setting a password for ease of testing.
-#Removes anonymous users, diallows root login remotely
-#Removes test database and access to it, and reloads privilege tables.
 mysql_secure_installation << EOF
 
 n
@@ -56,7 +50,7 @@ Y
 Y
 EOF
 
-#Install PHP from repository
+#Install PHP from repo
 sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf install dnf-utils -y http://rpms.remirepo.net/enterprise/remi-release-8.rpm
 sudo dnf module reset php
@@ -67,9 +61,8 @@ sudo systemctl enable php-fpm
 sudo setsebool -P httpd_execmem 1
 sudo systemctl restart httpd
 
-
 #Create database for WordPress
-sudo mysql -u root -p << EOF
+sudo mysql -u root -p <<EOF
 CREATE DATABASE wordpress;
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY 'password';
@@ -78,21 +71,19 @@ exit
 EOF
 sudo systemctl restart httpd
 
-#Install WordPress, transfer files with rsync & make new directory for uploads
+#Install WordPress, transfer files with rsync and make new directory for upload
 sudo wget http://wordpress.org/latest.tar.gz
 sudo tar xzvf latest.tar.gz
-#sudo chown -R test:test wordpress
-sudo rsync -avP /~/wordpress/ /var/www/html/
+sudo rsync -avP ~/wordpress/ /var/www/html/
 sudo mkdir /var/www/html/wp-content/uploads
 sudo chown -R apache:apache /var/www/html/*
 
-#Connect WordPress to database/edit config file.
+#Connect WordPress to database/edit config file
 cd /var/www/html
 sudo cp wp-config-sample.php wp-config.php
 sudo sed -i 's/database\_name\_here/wordpress/' wp-config.php
 sudo sed -i 's/username\_here/admin/' wp-config.php
 sudo sed -i 's/password\_here/password/' wp-config.php
-
 
 
 
